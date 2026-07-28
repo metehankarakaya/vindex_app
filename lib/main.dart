c4902e3b64538761867d2ca7c3350b10ba639215
Metehan KARAKAYA <karakayametehan00@gmail.com>
2026-07-28 16:30:31 +0300

    feat: add easy_localization with English and Turkish translations

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vindex_app/router/app_router.dart';
 
-void main() {
-  runApp(ProviderScope(child: const MyApp()));
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('tr')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: ProviderScope(child: const MyApp()),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +24,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       routerConfig: appRouter,
     );
   }
