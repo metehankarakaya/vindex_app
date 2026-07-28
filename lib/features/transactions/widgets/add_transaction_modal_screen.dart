a7ad39d64abf2d065d0b7fa81e8dca939ba60ad5
Metehan KARAKAYA <karakayametehan00@gmail.com>
2026-07-28 20:39:46 +0300

    feat: connect recurring form to modal tab

--- lib/features/transactions/widgets/add_transaction_modal_screen.dart
+++ lib/features/transactions/widgets/add_transaction_modal_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:vindex_app/core/constants/app_strings.dart';
 import 'package:vindex_app/features/transactions/widgets/add_transaction_screen.dart';
 
+import '../../recurring/widgets/add_recurring_transaction_screen.dart';
+
 class AddTransactionModalScreen extends StatelessWidget {
   const AddTransactionModalScreen({super.key});
 
@@ -42,7 +44,7 @@ class AddTransactionModalScreen extends StatelessWidget {
                 physics: const NeverScrollableScrollPhysics(),
                 children: [
                   const AddTransactionScreen(),
-                  const Center(child: Text('Coming soon')),
+                  const AddRecurringTransactionScreen(),
                 ],
               ),
             ),
